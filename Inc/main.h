fac9d9e74e3dbe9509a5f76a6707a0b3de987596
fl0mll <mueller@mllapps.com>
2018-02-09 21:22:01 +0100

    first generated code

---
+++ Inc/main.h
@@ -0,0 +1,100 @@
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define LD1_OUT_Pin GPIO_PIN_13
+#define LD1_OUT_GPIO_Port GPIOC
+#define SW1_IN_Pin GPIO_PIN_0
+#define SW1_IN_GPIO_Port GPIOA
+#define MTR_MS1_Pin GPIO_PIN_3
+#define MTR_MS1_GPIO_Port GPIOA
+#define MTR_MS2_Pin GPIO_PIN_4
+#define MTR_MS2_GPIO_Port GPIOA
+#define MTR_MS3_Pin GPIO_PIN_5
+#define MTR_MS3_GPIO_Port GPIOA
+#define MTR_STEP_Pin GPIO_PIN_0
+#define MTR_STEP_GPIO_Port GPIOB
+#define MTR_DIR_Pin GPIO_PIN_1
+#define MTR_DIR_GPIO_Port GPIOB
+#define MTR_nENABLE_Pin GPIO_PIN_2
+#define MTR_nENABLE_GPIO_Port GPIOB
+#define MTR_nSLEEP_Pin GPIO_PIN_10
+#define MTR_nSLEEP_GPIO_Port GPIOB
+#define MTR_nRESET_Pin GPIO_PIN_11
+#define MTR_nRESET_GPIO_Port GPIOB
+#define DBG_CLI_TX_Pin GPIO_PIN_9
+#define DBG_CLI_TX_GPIO_Port GPIOA
+#define DBG_CLI_RX_Pin GPIO_PIN_10
+#define DBG_CLI_RX_GPIO_Port GPIOA
+
+/* ########################## Assert Selection ############################## */
+/**
+  * @brief Uncomment the line below to expanse the "assert_param" macro in the 
+  *        HAL drivers code
+  */
+/* #define USE_FULL_ASSERT    1U */
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+void _Error_Handler(char *, int);
+
+#define Error_Handler() _Error_Handler(__FILE__, __LINE__)
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
